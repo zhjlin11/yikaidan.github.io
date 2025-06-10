e34fd03b29fa2d537c331ebf75f18f3126e7e663
zhjlin11 <51261564+zhjlin11@users.noreply.github.com>
2025-06-10 08:50:49 +0800

    Implement order detail fetch, update and delete

--- miniprogram/pages/orderDetail/index.js
+++ miniprogram/pages/orderDetail/index.js
@@ -1,4 +1,58 @@
 Page({
-  data: {},
-  onLoad(options) {}
-})
+  data: {
+    orderId: null,
+    order: null
+  },
+
+  onLoad(options) {
+    const id = options.id;
+    if (id) {
+      this.setData({ orderId: id });
+      this.fetchOrder();
+    }
+  },
+
+  fetchOrder() {
+    wx.request({
+      url: `http://localhost:3000/orders/${this.data.orderId}`,
+      method: 'GET',
+      success: (res) => {
+        this.setData({ order: res.data });
+      }
+    });
+  },
+
+  onQuantityChange(e) {
+    this.setData({ 'order.quantity': e.detail.value });
+  },
+
+  onStatusChange(e) {
+    this.setData({ 'order.status': e.detail.value });
+  },
+
+  updateOrder() {
+    wx.request({
+      url: `http://localhost:3000/orders/${this.data.orderId}`,
+      method: 'PUT',
+      data: {
+        quantity: this.data.order.quantity,
+        status: this.data.order.status
+      },
+      success: (res) => {
+        this.setData({ order: res.data });
+        wx.showToast({ title: 'Updated', icon: 'success' });
+      }
+    });
+  },
+
+  deleteOrder() {
+    wx.request({
+      url: `http://localhost:3000/orders/${this.data.orderId}`,
+      method: 'DELETE',
+      success: () => {
+        wx.showToast({ title: 'Deleted', icon: 'success' });
+        wx.navigateBack();
+      }
+    });
+  }
+});
