33c13036a7f1349a8f0498dc9e6b1d49ce4846ce
zhjlin11 <51261564+zhjlin11@users.noreply.github.com>
2025-06-10 10:58:55 +0800

    Add Jest test framework and CRUD tests

--- server/index.js
+++ server/index.js
@@ -1,26 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const { sequelize } = require('./models');
-const customerRoutes = require('./routes/customers');
-const productRoutes = require('./routes/products');
-const orderRoutes = require('./routes/orders');
-const wechatRoutes = require('./routes/wechat');
-
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-
-app.use('/customers', customerRoutes);
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
-app.use('/wechat', wechatRoutes);
-
-app.use((err, req, res, next) => {
-  console.error(err)
-  res.status(500).json({ error: 'Internal server error' })
-})
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
