4b208fdf8cf85b3ad1e900bbaaec53f123c3547d
zhjlin11 <51261564+zhjlin11@users.noreply.github.com>
2025-06-10 08:50:03 +0800

    feat: add order creation and list features

--- miniprogram/pages/orderList/index.js
+++ miniprogram/pages/orderList/index.js
@@ -1,4 +1,22 @@
 Page({
-  data: {},
-  onLoad(options) {}
+  data: {
+    orders: []
+  },
+
+  onLoad() {
+    this.fetchOrders()
+  },
+
+  fetchOrders() {
+    wx.request({
+      url: 'http://localhost:3000/orders',
+      method: 'GET',
+      success: (res) => {
+        this.setData({ orders: res.data })
+      },
+      fail: () => {
+        wx.showToast({ title: '加载失败', icon: 'none' })
+      }
+    })
+  }
 })
